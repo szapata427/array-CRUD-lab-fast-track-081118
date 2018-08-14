b93e56754e2d84f1ba227ca67cbefa7a46a6d88f
Steven Zapata <szapata427@gmail.com>
2018-08-14 17:40:19 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -30,7 +30,13 @@ def retrieve_element_from_index(array, index_number)
     array[index_number]
  end
 
+def retrieve_first_element_from_array(array)
+  array.shift
+end
 
+def retrieve_last_element_from_array(array)
+  
+end
 
 
 
