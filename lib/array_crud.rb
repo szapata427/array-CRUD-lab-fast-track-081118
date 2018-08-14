f73a3c41546eb5fd730a15652105ce47ee1b5031
Steven Zapata <szapata427@gmail.com>
2018-08-14 17:38:10 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -29,8 +29,7 @@ end
 def retrieve_element_from_index(array, index_number)
     array = ["am", "am", "am"]
     array.[index_number]
-    
- end
+end
 
 def retrieve_first_element_from_array(array)
   
